d4e85ca41887f1f3a7f2f517769d6b2186697892
Amanda Gabriela <amangxc@gmail.com>
2024-08-02 09:37:04 -0300

    condição ternária

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,14 +1,9 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 8;
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <=7 ? "Recuperação" : "Reprovado";
 
-        if(nota >= 7)
-        System.out.println("Aprovado");
 
-        else if(nota >= 5 && nota < 7)
-        System.out.println("Prova Recuperação");
-        
-        else
-        System.out.println("Reprovado");
+        System.out.println(resultado);
     }
 }
